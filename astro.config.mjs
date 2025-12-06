fa710ec835a3648760e4ea10da99bcbea51fe679
Filippo Fornai <filippofornai1@gmail.com>
2025-12-06 15:24:55 +0100

    Added initial page organization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://filippo-fornai.github.io',
     base: '/ShaderCodingMadeEasy',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
